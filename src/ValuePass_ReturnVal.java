acefb59956e89e5202c21fbe9b8d9b7e6503fc99
Adrija Datta <adrijadatta@Adrijas-Laptop.local>
2025-10-12 01:53:53 +0530

    from AdMacAir

---
+++ src/ValuePass_ReturnVal.java
@@ -0,0 +1,18 @@
+package Methods.Functions;
+class Solution { int n,x ;
+}
+public class ValuePass_ReturnVal {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.n = 10;
+        returnValue(s);
+        System.out.println(s.x);
+    }
+    public static void returnValue(Solution s)
+    {
+        s = new Solution();
+        s.n = 10;
+        s.x = s.n*s.n;
+        System.out.println("inside method "+s.x);
+    }
+}
