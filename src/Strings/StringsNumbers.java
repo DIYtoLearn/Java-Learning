8e38f12a433963be2c3fe67b118be6724ffa9137
DIYtoLearn <upayanplayer007@gmail.com>
2025-10-12 21:56:14 +0530

    finishing up from here

--- src/Strings/StringsNumbers.java
+++ src/Strings/StringsNumbers.java
@@ -1,5 +1,31 @@
 package Strings;
 
 public class StringsNumbers {
-    
+    static void StringTONumber(String str)
+    {
+//        char c = str.charAt(0);
+//        int x = c;
+//        System.out.println(x);
+
+        int len = str.length();
+        for(int i=0; i<len; i++)
+        {
+            int z = str.charAt(i);
+            System.out.println(z);
+        }
+
+    }
+
+    static void PrintNumbertoCharacters(char c5, char c6)
+    {
+
+    }
+
+    public static void main(String[] args) {
+        String s = "Hello";
+        StringTONumber(s);
+
+        PrintNumbertoCharacters('a','h');
+
+    }
 }
