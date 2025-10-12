52652d211b9f6e0aaf6002e7f29513227a36425e
DIYtoLearn <upayanplayer007@gmail.com>
2025-10-13 01:53:43 +0530

    Fianl of 12th Oct

---
+++ src/Strings/PalindromeREVERSE.java
@@ -0,0 +1,22 @@
+package Strings;
+
+public class PalindromeREVERSE {
+
+    static void Palindrome(String s1)
+    {
+
+    }
+
+    static void reverse(String s2)
+    {
+
+
+    }
+
+    public static void main(String[] args) {
+        String chk = "TenEt";
+        Palindrome(chk);
+        reverse(chk);
+
+    }
+}
