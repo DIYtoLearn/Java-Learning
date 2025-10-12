467c04fc9ff21a4a444a78d2d811196dd6c8354b
DIYtoLearn <upayanplayer007@gmail.com>
2025-10-13 01:53:43 +0530

    Fianl of 12th Oct

--- src/Strings/StringsNumbers.java
+++ src/Strings/StringsNumbers.java
@@ -7,18 +7,29 @@ public class StringsNumbers {
 //        int x = c;
 //        System.out.println(x);
 
+        char xx = 65;
+        System.out.println(xx);
+
         int len = str.length();
         for(int i=0; i<len; i++)
         {
             int z = str.charAt(i);
             System.out.println(z);
         }
-
     }
 
     static void PrintNumbertoCharacters(char c5, char c6)
     {
+        int m = c5;
+        int m2 = c6;
+        //System.out.println(m2);
 
+        for(int i=m; i<=m2; i++)
+        {
+            char m3 = (char)i;
+            System.out.print(m3+" ");
+
+        }
     }
 
     public static void main(String[] args) {
