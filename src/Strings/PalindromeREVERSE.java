3edb4aee2ecbb6fae3499ac61ea1bfb92ba590c4
DIYtoLearn <upayanplayer007@gmail.com>
2025-10-14 11:50:53 +0530

    From personal

--- src/Strings/PalindromeREVERSE.java
+++ src/Strings/PalindromeREVERSE.java
@@ -9,12 +9,20 @@ public class PalindromeREVERSE {
 
     static void reverse(String s2)
     {
+        int len = s2.length();
+        String s3 = "";
+        System.out.println(s3);
 
+        for(int i=0; i<len; i++)
+        {
+            s3 = s3 + s2.charAt(i);
+        }
 
+        System.out.println(s3);
     }
 
     public static void main(String[] args) {
-        String chk = "TenEt";
+        String chk = "Hello";
         Palindrome(chk);
         reverse(chk);
 
