63384849aaabeaa0994e08213b35e48831c5ce95
DIYtoLearn <upayanplayer007@gmail.com>
2025-10-21 14:36:07 +0530

    counting and panagram

--- src/Strings/CharacterArraySORT.java
+++ src/Strings/CharacterArraySORT.java
@@ -1,5 +1,65 @@
 package Strings;
 
+import java.util.Arrays;
+
 public class CharacterArraySORT {
 
+    static void Bubble(String str)
+    {
+        char[] c = str.toCharArray();
+        int len = c.length;
+        char temp;
+        System.out.println(c);
+
+            for(int i=0; i<len; i++)
+            {
+                for(int j=0; j<len-i-1; j++)
+                {
+                    if(c[j] > c[j+1])
+                    {
+                        temp = c[j];
+                        c[j] = c[j+1];
+                        c[j+1] = temp;
+                    }
+                }
+            }
+
+            System.out.println(c);
+    }
+
+    static void selection(String str2)
+    {
+        char[] c2 = str2.toCharArray();
+        int len = c2.length;
+        int index;
+        char temp;
+        System.out.println(c2);
+
+            for(int i=0; i<len; i++)
+            {
+                index= i;
+                for(int j=i+1; j<len; j++)
+                {
+                    if(c2[j] < c2[index])
+                        index = j;
+                }
+                temp = c2[i];
+                c2[i] = c2[index];
+                c2[index] = temp;
+            }
+
+            System.out.println(c2);
+    }
+
+    static void counting(String str3)
+    {
+
+
+    }
+
+    public static void main(String[] args) {
+        Bubble("dba");
+        selection("check");
+        counting("laugh");
+    }
 }
