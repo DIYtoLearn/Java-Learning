00eaf4824507ec02fbb2bd0d6de5af241e2570a6
DIYtoLearn <upayanplayer007@gmail.com>
2025-10-31 15:21:19 +0530

    Learning

--- src/All_tests/Test/Inh.java
+++ src/All_tests/Test/Inh.java
@@ -1,8 +1,23 @@
 package All_tests.Test;
 
 public class Inh {
-    protected void fun()
+    
+     protected void fun()
     {
         System.out.println("This print statement is another package");
+    }
+
+    protected void using()
+    {
+        fun();
+    }
+}
+
+class another
+{
+    public static void main(String[] args) {
+        Inh it2 = new Inh();
+        it2.using();
+
     }
 }
