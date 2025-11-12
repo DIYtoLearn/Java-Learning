34766dfd57ffc6b048eb3a2bad6180e3f508df29
DIYtoLearn <upayanplayer007@gmail.com>
2025-11-12 18:44:54 +0530

    Learning

--- src/All_tests/Test/Max_Min.java
+++ src/All_tests/Test/Max_Min.java
@@ -9,7 +9,7 @@ class Solution2 {
 
         for(int i=1; i<arr.size(); i++)
         {
-            if(arr.get(MAX) < arr.get(i));
+            if(arr.get(MAX) < arr.get(i))
             MAX = i;
         }
 
@@ -20,16 +20,24 @@ class Solution2 {
     public static int minimumElement(ArrayList<Integer> arr) {
 
         // Your code here
-        return 2;
+        int MIN = 0;
+
+        for(int j=0; j<arr.size(); j++)
+        {
+            if(arr.get(MIN) > arr.get(j))
+                MIN = j;
+        }
+        return arr.get(MIN);
     }
 }
 
 public class Max_Min {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(10);
+        list.add(100);
         list.add(11);
-        list.add(2);
+        list.add(20);
+        list.add(111);
 
 
         int req1 = Solution2.maximumElement(list);
