22f1c757f0ef1421d283da1dbf3a8728b5a69b6d
DIYtoLearn <upayanplayer007@gmail.com>
2025-11-17 11:25:20 +0530

    Learning

---
+++ src/SmallTOPICS/BigINTEGER.java
@@ -0,0 +1,48 @@
+package SmallTOPICS;
+
+import java.math.BigInteger;
+
+class operation
+{
+    BigInteger bii;
+    operation(BigInteger _bii)
+    {
+        bii = _bii;
+    }
+
+    void OperationsPOSSIBLE()
+    {
+        BigInteger req = bii.add(bii);
+        System.out.println("Addition "+req);
+
+        req = bii.multiply(bii);
+        System.out.println("Multiplication "+req);
+
+        BigInteger bn = new BigInteger("2");
+        req = bii.divide(bn);
+        System.out.println("Divide "+req);
+
+        req = bii.subtract(bii);
+        System.out.println("Substraction "+req);
+
+    }
+}
+
+public class BigINTEGER {
+    public static void main(String[] args) {
+
+        BigInteger bi3 = new BigInteger("81");
+
+        int c = 134;
+        BigInteger bi2 = BigInteger.valueOf(c);
+
+        String str = "1432";
+        BigInteger bi = new BigInteger(str);
+
+        System.out.println(bi+" "+bi2+" "+bi3);
+
+        operation op = new operation(bi);
+        op.OperationsPOSSIBLE();
+    }
+}
+
