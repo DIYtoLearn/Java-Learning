172f27027398c0c7f049db78b64863e78fc1cbb6
DIYtoLearn <upayanplayer007@gmail.com>
2025-11-21 17:43:53 +0530

    Update

---
+++ src/GFG/PrimeCheck.java
@@ -0,0 +1,45 @@
+package GFG;
+import java.util.Scanner;
+
+class Numbers{
+
+    int number;
+    Numbers(int n)
+    {
+        number = n;
+        //System.out.println(n+" "+number);
+    }
+
+    protected boolean prime()
+    {
+
+        if(number <= 1)
+            return  false;
+        else if(number <= 3)
+            return true;
+        else {
+            for(int i=2; i<= Math.sqrt(number); i++)
+            {
+                if(number % i == 0)
+                    return false;
+
+            }
+            return  true;
+        }
+    }
+
+}
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        Numbers nm = new Numbers(x);
+        boolean req = nm.prime();
+        System.out.println(req);
+
+
+    }
+}
