fc20727de1731222f10bad755ffccca2bc759d7e
DIYtoLearn <upayanplayer007@gmail.com>
2025-11-23 13:00:34 +0530

    update

---
+++ src/SmallTOPICS/BigIntegers/BigIntegerPRIME.java
@@ -0,0 +1,27 @@
+package SmallTOPICS.BigIntegers;
+import java.math.BigInteger;
+
+class Solution2 {
+    public static int nextPrime(int n) {
+
+        BigInteger n2 = BigInteger.valueOf(n);
+
+        if(n2.compareTo(BigInteger.ONE) < 0)
+        {
+            return 2;
+        }
+
+        BigInteger next = n2.nextProbablePrime();
+        return next.intValue();
+
+    }
+}
+
+public class BigIntegerPRIME {
+    public static void main(String[] args) {
+
+        int req2 = Solution2.nextPrime(33);
+        System.out.println("Next Prime: "+req2);
+
+    }
+}
