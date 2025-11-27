83c554a26fc796286e37ba3e0e78a2806399c91d
DIYtoLearn <upayanplayer007@gmail.com>
2025-11-27 21:54:32 +0530

    update

--- src/Strings/test.java
+++ src/Strings/test.java
@@ -1,7 +1,30 @@
 package Strings;
 
-
+// Count the number of words in a given string
 public class test {
+
+    static void countWords()
+    {
+        String str;
+        //str = "Hello   world this is  Java   ";
+        str = "vnjb nsjpjbjlmbvrekhawnvrmpytfvdgqbjznxwruzlkkyi";
+        int count = 0;
+        boolean inWord = false;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                if (!inWord) {
+                    count++;
+                    inWord = true;
+                }
+            } else {
+                inWord = false;
+            }
+        }
+
+        System.out.println(count);
+
+    }
 
     public static void main(String[] args) {
 
@@ -11,24 +34,25 @@ public class test {
         int len = str.length();
         int x;
 
-        for(int i=0; i<len; i++)
-        {
+        for (int i = 0; i < len; i++) {
             x = str.charAt(i);
 
-            if(x == 32)
-            {
+            if (x == 32) {
                 space++;
                 count++;
             }
 
-            if(count != 0 && i == (len-1))
+            if (count != 0 && i == (len - 1))
                 count++;
         }
 
-        if(space == 0 && !str.isEmpty())
+        if (space == 0 && !str.isEmpty())
             count++;
 
-        System.out.println(count);
+        //System.out.println(count);
+
+        countWords();
+
     }
 }
 
