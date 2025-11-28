6a306b0222b1408635e4d15a5e95d1b995d59616
DIYtoLearn <upayanplayer007@gmail.com>
2025-11-28 06:07:30 +0530

    update

--- src/Strings/LongestPREFIX.java
+++ src/Strings/LongestPREFIX.java
@@ -1,5 +1,11 @@
 package Strings;
 
+/*
+HELLO , YELLOW, GALLON, MELLOW
+Longest prefix in all the words: "LLO"
+
+Our task is to find that longest prefix in a given string arrays
+ */
 
 
 public class LongestPREFIX {
