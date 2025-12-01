6ea63e8cc5f03b0a58f53f4349682d92e9b6b157
DIYtoLearn <upayanplayer007@gmail.com>
2025-12-01 23:17:09 +0530

    learn

---
+++ src/Strings/vowels.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class vowels {
+    public static void main(String[] args) {
+
+
+        String str = "check";
+        System.out.println(str.charAt(2));
+
+        char c = str.charAt(2);
+        if( c == 'e')
+        {
+            System.out.println("true");
+        }
+    }
+}
