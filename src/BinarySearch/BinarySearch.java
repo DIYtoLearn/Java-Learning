b8e46f23d3c6cefdecd6ecc5c0c1c76bba3ec659
DIYtoLearn <upayanplayer007@gmail.com>
2025-12-04 19:44:51 +0530

    update

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -10,18 +10,67 @@ class Search
         boolean found = false;
         while(true)
         {
-            if(mid == target)
+            if(arr[mid] == target)
             {
+                System.out.println("index = "+mid);
                 found = true;
                 break;
             }
 
             else {
-                
-
+                if(high < low)
+                {
+                    System.out.println("NOT FOUND");
+                    break;
+                }
+                if(arr[mid] < target)
+                {
+                    low = mid + 1;
+                    mid = (low + high) / 2;
+//                    if(arr[high] == target || arr[low] == target)
+//                    {
+//                        System.out.println("index "+high+" index2 "+low);
+//                        found = true;
+//                        break;
+//
+//                    }
+                }
+                else if(arr[mid] > target)
+                {
+                    high = mid - 1;
+                    mid = (low + high) / 2;
+//                    if(arr[high] == target || arr[low] == target)
+//                    {
+//                        System.out.println("index "+high+" index2 "+low);
+//                        found = true;
+//                        break;
+//
+//                    }
+                }
             }
-
         }
+
+        System.out.println(found);
+    }
+
+    protected void bs2GPT(int target, int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+        boolean found = false;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                found = true;
+                break;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println(found);
     }
 }
 
