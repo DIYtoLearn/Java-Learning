4c5434ed92ecc6fdba370fd02b9ed0630b3c6543
DIYtoLearn <upayanplayer007@gmail.com>
2025-12-06 14:50:15 +0530

    update

--- src/GFG/TwoSum.java
+++ src/GFG/TwoSum.java
@@ -1,30 +1,31 @@
 package GFG;
 
-public class TwoSum {
-    public static void main(String[] args) {
+public class TwoSum
+{
+    public static void main(String[] args)
+    {
         int i2 = -1;
         int j2 = 2;
         int c = 0;
         c = i2 + j2;
         //System.out.println(c);
 
-        int[] arr = {0, -1, 2, -3, 1};
+        int[] arr = {-1, 0, 1, 2, -1, -4};
                 //{1, -2, 1, 0, 5};
         //{0, -1, 2, -3, 1};
         int target = -2;
+        int target2 = 0;
         boolean bool = false;
 
                 int pair_sum = 0;
                 for (int i = 0; i < arr.length - 1; i++)
                 {
-                    if(bool == true)
-                        break;
-
                     for(int j=i+1; j < arr.length ; j++)
                     {
                         pair_sum = arr[i] + arr[j];
-                        if(pair_sum == target)
+                        if(pair_sum == target2)
                         {
+                            System.out.println(Math.max(arr[i],arr[j])+" "+Math.min(arr[i],arr[j]));
                             bool = true;
                             break;
                         }
