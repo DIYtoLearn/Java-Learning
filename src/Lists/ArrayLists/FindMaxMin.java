8b678870102c157774f40b8851b765f3b2364a81
DIYtoLearn <upayanplayer007@gmail.com>
2025-12-23 01:07:20 +0530

    update

--- src/Lists/ArrayLists/FindMaxMin.java
+++ src/Lists/ArrayLists/FindMaxMin.java
@@ -6,18 +6,34 @@ class AllSolution {
     public static int maximumElement(ArrayList<Integer> arr) {
         // Your code here
         ListIterator<Integer> it = arr.listIterator();
+        int max = Integer.MIN_VALUE;
 
         while(it.hasNext())
         {
-            
+            int compare_element = it.next();
+                if(compare_element > max)
+                    max = compare_element;
 
         }
 
-
+        return max;
+        
     }
 
     public static int minimumElement(ArrayList<Integer> arr) {
         // Your code here
+        ListIterator<Integer> it = arr.listIterator();
+        int min = Integer.MAX_VALUE;
+
+        while(it.hasNext())
+        {
+            int compare_element = it.next();
+                if(compare_element < min)
+                    min = compare_element;
+
+        }
+
+        return min;
     }
 }
 
@@ -32,6 +48,8 @@ public class FindMaxMin {
        int max = AllSolution.maximumElement(arr);
        int min = AllSolution.minimumElement(arr);
 
+       System.out.println("MAX in the list "+max);
+        System.out.println("MIN in the list "+min);
 
     }
 }
