1bd1bd09502c998fa68f2bd39bfef49fec2fe051
DIYtoLearn <upayanplayer007@gmail.com>
2025-12-24 21:29:48 +0530

    update

--- src/Lists/Test/Ideas.java
+++ src/Lists/Test/Ideas.java
@@ -1,7 +1,17 @@
 package Lists.Test;
 
+import java.util.ArrayList;
+
 public class Ideas {
     public static void main(String[] args) {
+        ArrayList < Integer> lst = new ArrayList<>();
+
+        lst.add(1);
+
+        lst.add(2);
+
+        //Printing Statement
+        System.out.println(lst.toArray()[0]);
 
     }
 }
