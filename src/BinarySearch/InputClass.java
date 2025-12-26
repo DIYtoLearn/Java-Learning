46465a36e0d67651b1c2dd3cc75e599f279f584f
DIYtoLearn <upayanplayer007@gmail.com>
2025-12-26 21:55:35 +0530

    finish the BS with arraylist

--- src/BinarySearch/InputClass.java
+++ src/BinarySearch/InputClass.java
@@ -2,12 +2,12 @@ package BinarySearch;
 
 import java.util.Scanner;
 
-class InputClass {
+public class InputClass {
     int[] arr = {1, 1, 1, 1, 2};
             //{2,4,6,7,9,10,11,23,45,446};
     String[] arr2;
 
-    protected int[] Input()
+    public int[] Input()
     {
         Scanner sc = new Scanner(System.in);
         int special = Integer.MAX_VALUE;
---
+++ src/Lists/ArrayLists/BinarySearchWithArralist.java
@@ -0,0 +1,11 @@
+package Lists.ArrayLists;
+
+import BinarySearch.InputClass;
+
+public class BinarySearchWithArralist {
+    public static void main(String[] args) {
+
+        InputClass ic = new InputClass();
+        int[] in = ic.Input();
+    }
+}
