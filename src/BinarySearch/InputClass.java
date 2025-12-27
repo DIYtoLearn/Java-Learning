ead724dd141b5942b7136ac33e1435a56472ec88
DIYtoLearn <upayanplayer007@gmail.com>
2025-12-27 20:28:17 +0530

    done

--- src/BinarySearch/InputClass.java
+++ src/BinarySearch/InputClass.java
@@ -3,7 +3,9 @@ package BinarySearch;
 import java.util.Scanner;
 
 public class InputClass {
-    int[] arr = {1, 1, 1, 1, 2};
+    int[] arr = {1, 3, 4, 5, 6};
+            //{1, 2, 3, 4, 6};
+            //{1, 1, 1, 1, 2};
             //{2,4,6,7,9,10,11,23,45,446};
     String[] arr2;
 
--- src/Lists/ArrayLists/BinarySearchWithArralist.java
+++ src/Lists/ArrayLists/BinarySearchWithArralist.java
@@ -1,11 +1,58 @@
 package Lists.ArrayLists;
 
 import BinarySearch.InputClass;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Search
+{
+    protected void operation(ArrayList<Integer> list)
+    {
+        System.out.println(list);
+        int low = 0;
+        int high = list.size()-1;
+        boolean found = false;
+        int target = 5;
+        int index = -1;
+
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(list.get(mid) == target){
+                found = true;
+                index = mid;
+                break;
+            }
+            else if(list.get(mid)>target)
+            {
+                high = mid-1;
+            }
+            else{
+                low = mid+1;}
+
+        }
+        System.out.println("The taget element is "+found+" and the index is "+index);
+        }
+
+    }
 
 public class BinarySearchWithArralist {
     public static void main(String[] args) {
 
         InputClass ic = new InputClass();
         int[] in = ic.Input();
+        //System.out.println(Arrays.toString(in));
+
+        // Convert int[] to List<Integer>
+        List<Integer> list = Arrays.stream(in)      // Create an IntStream
+                .boxed()                            // Box int to Integer
+                .collect(Collectors.toList());
+
+        //System.out.println(list);
+
+        Search sc = new Search();
+        sc.operation((ArrayList<Integer>) list);
     }
 }
