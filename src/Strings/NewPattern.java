e3b18f3472d26c4559ec3d57fcc6bf67ea574044
DIYtoLearn <upayanplayer007@gmail.com>
2025-12-30 19:31:21 +0530

    complete

---
+++ src/Strings/NewPattern.java
@@ -0,0 +1,48 @@
+package Strings;
+
+class Geeks {
+     int follPatt(String s) {
+        // Your code here
+        int[] arr = new int[26];
+        //int diff_cal = 'a';
+
+        int len = s.length();
+        //int present_integerValue_of_char;
+        //char convert;
+        //int counterForX = 0;
+        //int counterForY = 0;
+
+        if(s.charAt(0) == 'y')
+            return 0;
+
+        while(int i<len)
+        {
+            if(s.charAt(i) == 'x')
+                arr[23]++;
+
+
+            if(s.charAt(i) == 'y')
+            {
+                for(int j=0;j<arr[23];j++)
+                {
+                    if(s.charAt(i) != 'y')
+                        return 0;
+
+                    i++;
+                }
+
+                arr[23] = 0;
+
+            }
+            i++;
+        }
+
+        return 1;
+    }
+}
+
+public class NewPattern {
+    public static void main(String[] args) {
+
+    }
+}
