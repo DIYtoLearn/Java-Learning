983628caaecb352a1a1b613fd135c762176a0323
DIYtoLearn <upayanplayer007@gmail.com>
2026-01-01 11:57:22 +0530

    Happy New Year!

--- src/Contests/CountingLockers.java
+++ src/Contests/CountingLockers.java
@@ -5,28 +5,29 @@ class HighSchoolLockers
     public static int closedLockers(int n, int m)
     {
         int[] arr = new int[n];
-        int len = n/m;
-        int count=0;
+        int len;
 
+        if(m==0)
+            return n;
+        else
+            len = n/m;
+
+        int count=0;
 
         for(int i=1; i<=len; i++)
         {
-            arr[(2*i)-1]++;
+            arr[(m*i)-1]++;
+            count++;
         }
 
-        for(int x:arr){
-           if(x==0)
-               count++;
-        }
-
-        return count;
+        return (n-count);
 
     }
 }
 
 public class CountingLockers {
     public static void main(String[] args) {
-        int count_req = HighSchoolLockers.closedLockers(12,3);
+        int count_req = HighSchoolLockers.closedLockers(10,2);
         System.out.println("Closed Lockers "+count_req);
 
     }
