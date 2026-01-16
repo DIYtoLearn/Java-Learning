1ad6668bd67cbd7c0aa188d852db3a6c14383e73
DIYtoLearn <upayanplayer007@gmail.com>
2026-01-16 19:03:36 +0530

    OOPS

---
+++ src/Concepts/OOPS/Procedural.java
@@ -0,0 +1,31 @@
+package Concepts.OOPS;
+
+class complex{
+    int real, imaginary;
+
+    complex(int real, int imaginary)
+    {
+        this.real = real;
+        this.imaginary = imaginary;
+
+    }
+
+    void print()
+    {
+        System.out.println(real +" i"+imaginary);
+    }
+
+    void Add(complex c)
+    {
+        real = real + c.real;
+        imaginary = imaginary + c.imaginary;
+    }
+}
+
+public class Procedural {
+    public static void main(String[] args) {
+        complex c1 = new complex(10,20);
+        c1.print();
+
+    }
+}
