cfa68b1ab5beae6c67f7c3ef35548e4e590cfe3f
DIYtoLearn <upayanplayer007@gmail.com>
2026-01-16 22:13:58 +0530

    update

--- src/Concepts/OOPS/Procedural.java
+++ src/Concepts/OOPS/Procedural.java
@@ -1,31 +1,35 @@
 package Concepts.OOPS;
 
-class complex{
-    int real, imaginary;
-
-    complex(int real, int imaginary)
+    class complex
     {
-        this.real = real;
-        this.imaginary = imaginary;
+        int real, imaginary;
 
-    }
+        complex(int real, int imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
 
-    void print()
-    {
-        System.out.println(real +" i"+imaginary);
-    }
+        }
 
-    void Add(complex c)
-    {
-        real = real + c.real;
-        imaginary = imaginary + c.imaginary;
+        void print()
+        {
+            System.out.println(real +" i"+imaginary);
+        }
+
+        void Add(complex c)
+        {
+            real = real + c.real;
+            imaginary = imaginary + c.imaginary;
+        }
     }
-}
 
 public class Procedural {
     public static void main(String[] args) {
         complex c1 = new complex(10,20);
         c1.print();
+        c1.Add(new complex(30,20));
+        c1.print();
+
 
     }
 }
