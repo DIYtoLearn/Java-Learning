0e81a164110c8a34c4e29866119ca743cc573668
DIYtoLearn <upayanplayer007@gmail.com>
2026-01-18 20:48:18 +0530

    update

--- src/NewIDEAS/FileRead2.java
+++ src/NewIDEAS/FileRead2.java
@@ -38,10 +38,17 @@ class Reader
 }
 
 public class FileRead2 {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args)  {
 
         Reader rr = new Reader();
-        int val = rr.readfromFile();
+        int val=0;
+        try {
+            val = rr.readfromFile();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Check if the file path or if the file is present at the desired location !!");
+        }
 
         System.out.println("Final Sum = "+val);
         System.out.println("We got this via get method "+rr.getClassvariable());
