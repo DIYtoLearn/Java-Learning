62c42e852e68bd768e0e8963aad318772eabb6e3
DIYtoLearn <upayanplayer007@gmail.com>
2026-01-19 18:55:41 +0530

    update

--- src/NewIDEAS/FileRead2.java
+++ src/NewIDEAS/FileRead2.java
@@ -1,41 +1,40 @@
 package NewIDEAS;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 class Reader
-{
-    private static int classvariable ;
-    static {
-        classvariable = 0;
-    }
-
-    protected int getClassvariable()
     {
-        return classvariable;
-    }
-
-    protected void readfromFile() throws Exception
-    {
-        Scanner sc = new Scanner(new File("/Users/admin/Desktop/read1"));
-        // For Mac "/Users/admin/Desktop/read"
-        // For windows "C:\\Users\\Upayan\\Desktop\\read.txt"
-        int req = 0;
-        while(sc.hasNext()) {
-            int each = sc.nextInt();
-            System.out.println(each);
-            req = returnSumInFile(each);
+        private static int classvariable ;
+        static {
+            classvariable = 0;
         }
-        System.out.println("Final Sum = "+req);
-    }
 
-    protected static int returnSumInFile(int n)
-    {
-         classvariable += n;
-         return classvariable;
+        protected int getClassvariable()
+        {
+            return classvariable;
+        }
+
+        protected void readfromFile() throws Exception
+        {
+            Scanner sc = new Scanner(new File("/Users/admin/Desktop/read1"));
+            // For Mac "/Users/admin/Desktop/read"
+            // For windows "C:\\Users\\Upayan\\Desktop\\read.txt"
+            int req = 0;
+            while(sc.hasNext()) {
+                int each = sc.nextInt();
+                System.out.println(each);
+                req = returnSumInFile(each);
+            }
+            System.out.println("Final Sum = "+req);
+        }
+
+        protected static int returnSumInFile(int n)
+        {
+             classvariable += n;
+             return classvariable;
+        }
     }
-}
 
 public class FileRead2 {
     public static void main(String[] args)  {
---
+++ src/SmallTOPICS/ExceptionHandling/ShowcaseTHROW.java
@@ -0,0 +1,29 @@
+package SmallTOPICS.ExceptionHandling;
+
+class throwshow
+{
+    static int divide(int a, int b) {
+        if(b==0)
+            throw new ArithmeticException("Cannot divide by 0");
+        return a/b;
+    }
+
+}
+
+public class ShowcaseTHROW {
+    public static void main(String[] args) {
+        int output = 0;
+        try{
+            output = throwshow.divide(10,5);
+            System.out.println(output);
+            output = throwshow.divide(10,0);
+            System.out.println(output);
+        }
+        catch (ArithmeticException e)
+        {
+            System.out.println("Does not make sense, "+e.getMessage());
+        }
+
+    }
+
+}
