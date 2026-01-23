e97789399e9e5b86db1a49627af047891f6c765f
DIYtoLearn <upayanplayer007@gmail.com>
2026-01-23 09:01:12 +0530

    update

--- src/SmallTOPICS/FileHandling/WrteToFile.java
+++ src/SmallTOPICS/FileHandling/WrteToFile.java
@@ -9,7 +9,7 @@ public class WrteToFile {
         try {
             FileWriter flw = new FileWriter("/Users/admin/Desktop/Upayan_Docs/Java_Practise/src/SmallTOPICS/FileHandling/Custom.txt",true);
 
-            flw.write("bubu ?\n"); // This overwrote my previous comment inside the file, need to figure out a way to stop that
+            flw.write("Hello File ! ?\n"); // This overwrote my previous comment inside the file, need to figure out a way to stop that
             // On multiple Run of this code it writing the above line on the first line of the text file, nowhere else !!
             // Even if I write a line in the 4th or 5th line of the text file when running this code it is making changes to the whole file and adding the above line in the 1st part of the text file
 
