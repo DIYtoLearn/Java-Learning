7fa0fc5242104de6ba347fd9fa293f09d9a8afc5
DIYtoLearn <upayanplayer007@gmail.com>
2026-03-23 17:54:55 +0530

    update

--- src/CollectionsKNOWLEDGE/OptionsInCollectionInterface.java
+++ src/CollectionsKNOWLEDGE/OptionsInCollectionInterface.java
@@ -1,5 +1,6 @@
 package CollectionsKNOWLEDGE;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.ListIterator;
 
 public class OptionsInCollectionInterface {
@@ -12,15 +13,25 @@ public class OptionsInCollectionInterface {
         Alst.add(21);
         Alst.add(29);
 
+        // ListIterator
         ListIterator<Integer> lit = Alst.listIterator();
+        System.out.println("Used ListIterator which is best of lists");
         while(lit.hasNext())
         {
             System.out.print(lit.next()+" ");
         }
 
+        Iterator<Integer> it = Alst.iterator();
+        System.out.println("\nUsed Normal Iterator that is provided with the Interface Iterable");
+        while(it.hasNext()){
+            System.out.print(it.next()+" ");
+        }
+
+
         int size = Alst.size();
         boolean checkEmpty = Alst.isEmpty();
         boolean IsPresent = Alst.contains(230);
+        Object[] arr = Alst.toArray();
 
         System.out.println("\nDesired Output = "+IsPresent);
 
--- src/hello-node/app.js
+++
@@ -1,5 +0,0 @@
-const greeting = "Hello, World!";
-console.log(greeting);
-
-// Let's add a little spice
-console.log("Node.js is running on your M1 Mac successfully!");
