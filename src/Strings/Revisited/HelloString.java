8fa314cefc7aad5862d209e5974af082f4a1e913
DIYtoLearn <upayanplayer007@gmail.com>
2026-04-25 15:57:23 +0530

    update

--- src/Strings/Revisited/HelloString.java
+++ src/Strings/Revisited/HelloString.java
@@ -1,5 +1,7 @@
 package Strings.Revisited;
 
+import java.util.Locale;
+
 class RevisitingAllProblems{
 
     public static void LengthOfString(String s)
@@ -19,20 +21,43 @@ class RevisitingAllProblems{
         }
         catch (Exception e)
         {
-            e.printStackTrace();
+            //e.printStackTrace();
             System.out.println("chodna");
             NormalFlow(NumberOfCharacters);
+            CountingCharacters(s,NumberOfCharacters);
         }
     }
-
+    // Panagram or one extra character
     private static void NormalFlow(int n)
     {
         System.out.println("Length of a string without using any method = "+n);
+    }
+
+    protected static void CountingCharacters(String s, int NumberOfCharacters)
+    {
+        int[] countArray = new int[26];
+        s = s.toLowerCase();
+        System.out.println(s);
+        int subtract = 'a';
+        int to_be_subtracted_from;
+        for(int i=0; i<NumberOfCharacters; i++){
+            if(!Character.isWhitespace(s.charAt(i))){
+            to_be_subtracted_from = s.charAt(i);
+            countArray[to_be_subtracted_from-subtract]++; }
+        }
+
+        for(int x : countArray) {
+            System.out.print(x+" ");
+        }
+    }
+
+    protected static void ExtraCharacter(int[] arr){
+
     }
 }
 
 public class HelloString {
     static void main() {
-        RevisitingAllProblems.LengthOfString("DeatHerTZ"); // You cannot safely check bounds without length — only workaround is exception handling
-    }
+        RevisitingAllProblems.LengthOfString("The quick brown fox jumps over the lazy dog"); // You cannot safely check bounds without length — only workaround is exception handling
+    } // DeatHerTZ
 }
