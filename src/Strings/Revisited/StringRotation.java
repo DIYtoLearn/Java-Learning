c4534a958ba651ffd21b1c6f6f3b794c0551a6a6
DIYtoLearn <upayanplayer007@gmail.com>
2026-04-25 17:47:27 +0530

    update

--- src/Strings/Revisited/StringRotation.java
+++ src/Strings/Revisited/StringRotation.java
@@ -2,4 +2,20 @@ package Strings.Revisited;
 
 // https://www.geeksforgeeks.org/batch/jbdl-blr-8/track/Java-Collections-String/problem/check-if-strings-are-rotations-of-each-other-or-not-1587115620
 public class StringRotation {
+    public static void main(String[] args) {
+        StringBuffer first = new StringBuffer("GFG");
+
+        System.out.println(first.toString().indexOf(" "));
+
+
+        String a = null;
+        String b = null;
+        try {
+            System.out.println(a.contains(b));
+        }
+        catch (NullPointerException e){
+            System.out.println("Knull hogaya !");
+
+        }
+    }
 }
