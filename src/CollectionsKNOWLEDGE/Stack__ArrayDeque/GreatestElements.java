3e9a5195e00052ae0ab400b1842e2d818bc3f2da
DIYtoLearn <upayanplayer007@gmail.com>
2026-05-04 14:45:18 +0530

    update

--- src/CollectionsKNOWLEDGE/Stack__ArrayDeque/GreatestElements.java
+++ src/CollectionsKNOWLEDGE/Stack__ArrayDeque/GreatestElements.java
@@ -1,6 +1,8 @@
 package CollectionsKNOWLEDGE.Stack__ArrayDeque;
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Deque;
 
 class AllSolutions
 {
@@ -28,10 +30,83 @@ class AllSolutions
             }
             if(count == 0)
                 als.add(no_greater_element);
+        }
+        return als.toArray();
+    }
 
+    protected void ClosestGreatestElementToTheLeftUsingStack(int[] arr){
+
+        Deque<Integer> Adq = new ArrayDeque<>();
+
+        Adq.push(arr[0]);
+        int previous_greatest_element = -1;
+        System.out.print(previous_greatest_element+" ");
+
+        for (int i=1; i<arr.length; i++) {
+            while (!Adq.isEmpty() && arr[i] >= Adq.peek()) {
+                Adq.pop();
+            }
+
+            previous_greatest_element = (Adq.isEmpty()) ? -1 : Adq.peek();
+            System.out.print(previous_greatest_element + " ");
+            Adq.push(arr[i]);
+        }
+    }
+
+    protected void NextGreatestElementToTheRightUsingStack(int[] arr)
+    {
+        Deque<Integer> Adq2 = new ArrayDeque<>();
+        Adq2.push(arr[arr.length-1]);
+        int next_greatest_element = -1;
+        int[] res = new int[arr.length];
+        res[res.length-1] = next_greatest_element;
+
+        for(int i=arr.length-2; i>=0; i--)
+        {
+            while(!Adq2.isEmpty() && arr[i] >= Adq2.peek())
+            {
+                Adq2.pop();
+            }
+
+            next_greatest_element = (Adq2.isEmpty()) ? -1 : Adq2.peek();
+            Adq2.push(arr[i]);
+            res[i] = next_greatest_element;
         }
 
-        return als.toArray();
+        System.out.println(Arrays.toString(res));
+    }
+
+    protected Object[] NextGreatestElementToTheRight(int[] arr)
+    {
+         // Input:  arr[] = {5, 15, 10, 8, 6, 12, 7}
+         // Output:           15 -1 12 12 12 -1 -1
+
+        // Input:  arr[] = {10, 15, 20, 25}
+        // Output:          15 20 25 -1
+
+        // Input:  arr[] = {25, 20, 15, 10}
+        // Output:         -1 -1 -1 -1
+
+        ArrayList<Integer> als2 = new ArrayList<>();
+        int current_element;
+        int no_next_greater_element_to_the_right = -1;
+        int count = 0;
+        for(int i=0; i<arr.length; i++)
+        {
+            current_element = arr[i];
+            count = 0;
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[j] > current_element) {
+                    als2.add(arr[j]);
+                    count++;
+                    break;}
+            }
+
+            if(count == 0)
+                als2.add(no_next_greater_element_to_the_right);
+        }
+        return als2.toArray();
     }
 }
 
@@ -40,9 +115,15 @@ public class GreatestElements {
     {
         System.out.println("Hello world");
         AllSolutions alsoln = new AllSolutions();
-        int[] arr = {15, 10, 18, 12, 4, 6, 2, 8};
+        int[] arr = {5, 15, 10, 8, 6, 12, 7};
+        System.out.println(Arrays.toString(arr));
         Object[] result = alsoln.ClosestGreaterElement(arr);
-        System.out.println(Arrays.toString(result));
+        //System.out.println(Arrays.toString(result));
+        Object[] arr2 = alsoln.NextGreatestElementToTheRight(arr);
+        //System.out.println(Arrays.toString(arr2));
+        alsoln.ClosestGreatestElementToTheLeftUsingStack(arr);
+        System.out.println();
+        alsoln.NextGreatestElementToTheRightUsingStack(arr);
 
     }
 }
