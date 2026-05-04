52422a2ff3cf6d59ac0c07362a5483326c030e36
DIYtoLearn <upayanplayer007@gmail.com>
2026-05-05 01:40:50 +0530

    update

--- src/NewIDEAS/FileRead2.java
+++ src/NewIDEAS/FileRead2.java
@@ -17,7 +17,7 @@ class Reader
 
         protected void readfromFile() throws Exception
         {
-            Scanner sc = new Scanner(new File("/Users/admin/Desktop/Sum.txt"));
+            Scanner sc = new Scanner(new File("/Users/admin/Desktop/Java-Learning/src/NewIDEAS/Sum.txt"));
             // For Mac "/Users/admin/Desktop/read"
             // For windows "C:\\Users\\Upayan\\Desktop\\read.txt"
             int req = 0;
