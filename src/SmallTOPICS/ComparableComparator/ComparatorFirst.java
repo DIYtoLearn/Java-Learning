82ff198e81b269ca4cc2b0d64019f55f75b66324
DIYtoLearn <upayanplayer007@gmail.com>
2026-05-08 14:31:47 +0530

    update

--- src/SmallTOPICS/ComparableComparator/ComparatorFirst.java
+++ src/SmallTOPICS/ComparableComparator/ComparatorFirst.java
@@ -8,14 +8,13 @@ public class ComparatorFirst {
     {
         ArrayList<Integer> arr = new ArrayList<>();
 
-        arr.add(10);
-        arr.add(-11);
-        arr.add(11);
+        arr.add(10); arr.add(7);
+        arr.add(-11); arr.add(11);
         arr.add(11); arr.add(-7122133);
         arr.add(15); arr.add(0); arr.add(91122);
-        arr.add(7);
         System.out.print(arr+"\n");
 
+        // Sorting the ArrayList of Numbers based ascending order
         Comparator<Integer> comp = (o1, o2) -> {
            if(o1 > o2)
                return 1;
@@ -26,7 +25,26 @@ public class ComparatorFirst {
         };
 
         arr.sort(comp);
-        System.out.print("ascending sort: "+arr);
+        System.out.println("ascending sort: "+arr);
+
+        ArrayList<String> AString = new ArrayList<String>();
+        AString.add("");  AString.add("length12");
+        AString.add("length1");  AString.add("length");
+        AString.add("length1234");  AString.add("len");
+
+        System.out.println(AString);
+
+        Comparator<String> comp2 = (String O1, String O2) ->{
+            if(O1.length() > O2.length())
+                return 1;
+            else if(O1.length() < O2.length())
+                return -1;
+            else
+                return 0;
+        };
+
+        AString.sort(comp2); // For  AString.add(null); getting null pointer exception not possible to sort this !
+        System.out.println("Sort based in String Length: "+AString);
 
     }
 }
