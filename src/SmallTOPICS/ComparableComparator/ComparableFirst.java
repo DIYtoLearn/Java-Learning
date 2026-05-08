5348104181e90752f06238c1b25d24e0a65b3f3c
DIYtoLearn <upayanplayer007@gmail.com>
2026-05-09 03:20:46 +0530

    update

--- src/SmallTOPICS/ComparableComparator/ComparableFirst.java
+++ src/SmallTOPICS/ComparableComparator/ComparableFirst.java
@@ -4,34 +4,34 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-class Student implements Comparable<Student>
-{
-    int age;
-    String name;
-
-    Student(int age, String name)
+    class Student implements Comparable<Student>
     {
-        this.age = age;
-        this.name = name;
-    }
+        int age;
+        String name;
 
-    @Override
-    public String toString()
-    {
-        return "Student [age= "+age +", name= " +name+"]";
+        Student(int age, String name)
+        {
+            this.age = age;
+            this.name = name;
+        }
 
+        @Override
+        public String toString()
+        {
+            return "Student [age= "+age +", name= " +name+"]";
+
+        }
+        // Sort the student's by increasing order of Age
+        @Override
+        public int compareTo(Student that) {
+            if(this.age > that.age)
+                return 1;
+            else if(this.age == that.age)
+                return 0;
+            else
+                return -1;
+        }
     }
-    // Sort the student's by increasing order of Age
-    @Override
-    public int compareTo(Student that) {
-        if(this.age > that.age)
-            return 1;
-        else if(this.age == that.age)
-            return 0;
-        else
-            return -1;
-    }
-}
 
 public class ComparableFirst {
     static void main() {
---
+++ src/SmallTOPICS/ComparableComparator/ComparatorMethods.java
@@ -0,0 +1,53 @@
+package SmallTOPICS.ComparableComparator;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
+import java.util.List;
+
+class PowerLevel{
+    String character;
+    int power_rating;
+
+    public PowerLevel(String character, int power_rating)
+    {
+        this.character = character;
+        this.power_rating = power_rating;
+    }
+
+    public int getPowerRating()
+    {
+        return power_rating;
+    }
+    public String getCharacter()
+    {
+        return character;
+    }
+
+    @Override
+    public String toString()
+    {
+        return character+" has power rating of "+power_rating;
+    }
+}
+
+public class ComparatorMethods {
+    static void main() {
+        List<PowerLevel> APlvl = new ArrayList<>();
+        APlvl.add(new PowerLevel("Gintoki",77));
+        APlvl.add(new PowerLevel("Goku",98));
+
+        // Comparator for comparing according to power level
+
+
+        Collections.sort(APlvl,Comparator.comparing(APlvl));
+
+
+
+
+        System.out.println("MAX Power Rating is 100");
+        for(PowerLevel printIt : APlvl)
+        {
+            System.out.println(printIt.toString());
+        }
+    }
+}
