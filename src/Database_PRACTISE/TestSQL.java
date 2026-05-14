92f0421579c5bf9a8b0d388f65d09c6f74190c7c
DIYtoLearn <upayanplayer007@gmail.com>
2026-05-15 02:05:53 +0530

    update

--- src/Database_PRACTISE/TestSQL.java
+++ src/Database_PRACTISE/TestSQL.java
@@ -5,22 +5,22 @@ import java.sql.*;
 public class TestSQL {
     public static void main(String[] args) {
         // Replace 'your_database_name' with the actual name of your schema
-        String url = "JDBC:mysql://localhost:3306/Macs";
+        String url = "JDBC:mysql://localhost:3306/test";
         String user = "root";
         String password = "root";
 
         // Try-with-resources ensures the connection and statement close automatically
         try (Connection conn = DriverManager.getConnection(url, user, password);
              Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT t.name_of_customer FROM customers as t where customer_id = 4")) {
+             ResultSet rs = stmt.executeQuery("SELECT t.email FROM user as t where id = 1")) {
 
-            System.out.println("name_of_customer");
+            System.out.println("Email of Customer");
             System.out.println("----------------");
 
             // Loop through the results
             while (rs.next()) {
                 //int id = rs.getInt("customer_id");
-                String name = rs.getString("name_of_customer");
+                String name = rs.getString("email");
                 //int deleted = rs.getInt("Deleted");
 
                 System.out.println(name);
