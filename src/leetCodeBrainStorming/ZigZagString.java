e2436af6614ec3fd99740cafbd00071e0370955d
DIYtoLearn <upayanplayer007@gmail.com>
2026-07-22 17:38:19 +0530

    update

---
+++ src/leetCodeBrainStorming/ZigZagString.java
@@ -0,0 +1,20 @@
+package leetCodeBrainStorming;
+
+// Very Bad Question Framing, No sense in solving this
+// https://leetcode.com/problems/zigzag-conversion/
+
+import java.util.Scanner;
+
+public class ZigZagString {
+    public static void main(String[] args) {
+
+        String s  = "";
+
+        Scanner sc = new Scanner(System.in);
+
+        s = sc.nextLine();
+        int len = s.length();
+
+        System.out.println(s);
+    }
+}
